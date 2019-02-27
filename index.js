7c7642bdd88663b3da779afebbc8da712ec7a20f
Yingqi Chen <chanwk18@hotmail.com>
2019-02-27 07:34:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -140,8 +140,12 @@ function moveDodger(e) {
    */
    if(e.which===LEFT_ARROW){
      moveDodgerLeft();
+     e.stopPropagation();
+     e.preventDefault();
    }else if(e.which===RIGHT_ARROW){
      moveDodgerRight();
+     e.stopPropagation();
+     e.preventDefault();
    }
 }
 
